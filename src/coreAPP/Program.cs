35383e529aa9cdce6866cd6329b542965a92754a
nakir <nakir@DESKTOP-VIJO5E1>
2016-12-22 00:17:21 -0500

    Add project files.

---
+++ src/coreAPP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreAPP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("this is a test");
+            Console.Read();
+
+        }
+    }
+}
